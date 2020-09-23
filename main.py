8cfcebb0c07b6eb9e36611405e79deb6921e5441
KatyaPanukova <panyukowa-yekaterina@yandex.ru>
2020-09-23 09:15:00 +0300

    start

---
+++ main.py
@@ -0,0 +1,20 @@
+import pygame                            # dedline 27.09.20 23:59:59
+from random import random
+from math import sqrt
+
+
+class Vector:
+    """Class vector"""
+    def __init__(self, ):
+        """Initialization method"""
+
+    def __add__(self, other):
+        """Summ of vectors"""
+
+    def __sub__(self, other):
+        """"""
+
+    def __mul__(self, other):
+        """"""
+
+    def len(self):
